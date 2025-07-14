82f4bc56c3577d8d47d92d5f82c55b636f9bc148
annalorena22 <annalorena1998@gmail.com>
2025-07-14 14:41:56 -0400

    feat(header): header para telas do menu (navbar), finalizada para componentizar

--- app/views/geral/home.php
+++ app/views/geral/home.php
@@ -1,15 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<?php
-    $css = [];
-    require_once('./utils/head.php')
-?>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <?php
+  $css = ['/css/header.css'];
+  require_once('./utils/head.php')
+  ?>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
 </head>
+
 <body>
-    <h1>ESTAMOS NA HOME!!!</h1>
+
+  <header class="header-container">
+    <div class="header-content">
+      <img src="public/images/logo.png" alt="MiniComunica" class="header-logo">
+    </div>
+  </header>
+
+  <h1>ESTAMOS NA HOME!!!</h1>
+  
 </body>
+
 </html>
