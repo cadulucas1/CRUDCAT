37ee3a83f59654f89279f342c2803fbdf4fb97be
Ricardo Sousa Silva <sousasilvaricardo9@gmail.com>
2025-07-15 09:20:54 -0400

    refactor: rafatorando script updae.sql, adicionando nova tabela relacionada e novas chaves

--- database/db_schema/db_schema.sql
+++ database/db_schema/db_schema.sql
@@ -9,7 +9,12 @@ CREATE TABLE produto IF NOT EXISTS produto(
 CREATE TABLE promocao IF NOT EXISTS promocao(
     id_promocao BIGINT AUTO_INCREMENT PRIMARY KEY,
     id_produto BIGINT NOT NULL,
-    foreign key fk_produto(id_produto) references produto(id_produto)
+    id_usuario BIGINT NOT NULL,
+    data_inicio BIGINT NOT NULL,
+    data_fim BIGINT NOT NULL,
+    percentual_desconto INT NOT NULL,
+    foreign key fk_produto(id_produto) references produto(id_produto),
+    foreign key fk_usuario(id_usuario) references usuario(id_usuario)
 )
 
 CREATE TABLE usuario IF NOT EXISTS usuario(
@@ -24,4 +29,16 @@ CREATE TABLE loja IF NOT EXISTS loja(
     id_loja BIGINT AUTO_INCREMENT PRIMARY KEY,
     nome_loja VARCHAR(255) NOT NULL,
     endereco_loja VARCHAR(255) NOT NULL,
+)
+
+CREATE TABLE loja_produto IF NOT EXISTS loja(
+    id_loja_produto BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id_loja BIGINT NOT NULL,
+    id_produto BIGINT NOT NULL,
+    quantidade INT NOT NULL,
+    preco_produto INT NOT NULL,
+    foreign key fk_loja(id_loja) references loja(id_loja),
+    foreign key fk_produto(id_produto) references produto(id_produto),
+    foreign key fk_preco(preco_produto) references produto(preco_produto)
+
 )
