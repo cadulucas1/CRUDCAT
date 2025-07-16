364cd64b668b66057360bdba7669431303a40cd0
annalorena22 <annalorena1998@gmail.com>
2025-07-16 17:30:24 -0400

    fix(giftcard): ajuste gift card na home

--- app/views/geral/home.php
+++ app/views/geral/home.php
@@ -1,26 +1,38 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <?php
-    $css = ['/css/geral/home.css', '/css/geral/navbar.css'];
-    require_once('./utils/head.php')
+$css = ['/css/geral/home.css', '/css/geral/navbar.css'];
+require_once('./utils/head.php')
 ?>
 </head>
+
 <body>
     <?php
-        include './app/components/php/navbar.php';
+    include './app/components/php/navbar.php';
     ?>
-    <?php 
-        // $topBarClass = 'estendida'; 
-        include './app/components/php/topbar.php'; 
+    <?php
+    // $topBarClass = 'estendida'; 
+    include './app/components/php/topbar.php';
     ?>
-    <main class="f-column" id="main-content">
-        <h2 class="title-store">
-            Lojas Seguidas
-        </h2>
-        <ul class="store-div">
-        </ul>
+
+    <main class="f-column">
+        <div class="store-div">
+            <gift-card></gift-card>
+        </div>
+        <div id="main-content">
+            <h2 class="title-store">
+                Lojas Seguidas
+            </h2>
+            <ul class="store-div">
+            </ul>
+
+        </div>
+
     </main>
-    
+
+
 </body>
 <script type="module" src="./public/js/geral/home.js"></script>
+<script type="module" src="./app/components/js/giftCard.js"></script>
+
 </html>
