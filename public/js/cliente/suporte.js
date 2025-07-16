9e16df95996a60e2694f0316ea45278437883b67
annalorena22 <annalorena1998@gmail.com>
2025-07-16 10:09:32 -0400

    fix(suporte cliente): arrumado botao de enviar mensagem

--- public/js/cliente/suporte.js
+++ public/js/cliente/suporte.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', () => {
     const formSuporte = document.querySelector('.form-suporte');
 
@@ -7,41 +6,27 @@ document.addEventListener('DOMContentLoaded', () => {
         return;
     }
 
-    const submitButton = formSuporte.querySelector('button[type="submit"]');
-    const originalButtonHtml = submitButton.innerHTML;
-
     formSuporte.addEventListener('submit', async (event) => {
         event.preventDefault();
 
         const formData = new FormData(formSuporte);
-        submitButton.disabled = true;
-        submitButton.textContent = 'Enviando...';
 
         try {
-           
             const response = await fetch('enviarSuporte', {
                 method: 'POST',
                 body: formData
             });
 
-            
             const data = await response.json();
 
             if (response.ok && data.sucesso) {
                 gerarToast(data.sucesso, 'sucesso');
-                submitButton.disabled = false;
-                submitButton.innerHTML = `
-                    ENVIAR MENSAGEM
-                    <img src="./public/images/icons/icon_enviar_mensagem.svg" alt="aviaozinho" class="icon-mensagem">
-                `;
                 formSuporte.reset();
             } else {
                 gerarToast(data.mensagem || 'Ocorreu um erro desconhecido.', 'erro');
-
             }
         } catch {
-            submitButton.disabled = false; 
-            submitButton.innerHTML = originalButtonHtml; 
+          return;  
         }
     });
 });
